0769342331c087cb250c5d5997a1605ca110c104
Jizhong Jiang <jiangjizhong@gmail.com>
2017-08-04 16:21:34 +0800

    support collect labels

---
+++ pilot/extension.go
@@ -0,0 +1,18 @@
+package pilot
+
+import (
+	"github.com/docker/docker/api/types"
+	"strings"
+	"fmt"
+)
+
+func extension(container map[string]string, containerJSON *types.ContainerJSON) {
+	labels := containerJSON.Config.Labels
+	for name, value := range labels {
+		if strings.HasPrefix(name, "com.aliyun.access.") {
+			fmt.Printf("label: %s=%s\n", name, value)
+			name = strings.Replace(name, ".", "_", -1)
+			putIfNotEmpty(container, name, value)
+		}
+	}
+}
