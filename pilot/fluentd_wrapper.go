0e923c37756d71c6252598a69761abf7b48962e7
bamboox <892078604@qq.com>
2017-08-05 14:26:04 +0800

    add ReloadFluentd monitor
    发送了SIGHUP，但是fluentd没有成功的重启worker进程

--- pilot/fluentd_wrapper.go
+++ pilot/fluentd_wrapper.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+	"time"
 )
 
 var fluentd *exec.Cmd
@@ -27,10 +28,35 @@ func StartFluentd() error {
 	return err
 }
 
+func shell(command string) string {
+	cmd := exec.Command("/bin/sh", "-c", command)
+	out, err := cmd.Output()
+	if err != nil {
+		fmt.Printf("error %v", err)
+	}
+	return string(out)
+}
+
 func ReloadFluentd() error {
 	if fluentd == nil {
 		return fmt.Errorf("fluentd have not started")
 	}
 	log.Warn("reload fluentd")
-	return fluentd.Process.Signal(syscall.SIGHUP)
+	ch := make(chan struct{})
+	go func(pid int) {
+		command := fmt.Sprintf("pgrep -P %d", pid)
+		childId := shell(command)
+		log.Infof("before reload childId : %s", childId)
+		fluentd.Process.Signal(syscall.SIGHUP)
+		time.Sleep(5 * time.Second)
+		afterChildId := shell(command)
+		log.Infof("after reload childId : %s", childId)
+		if childId == afterChildId {
+			log.Infof("kill childId : %s", childId)
+			shell("kill -9 " + childId)
+		}
+		close(ch)
+	}(fluentd.Process.Pid)
+	<-ch
+	return nil
 }
